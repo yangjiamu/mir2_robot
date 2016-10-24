f7ed32dc26cfa3bc093be36870543f02388b7531
yangwenjie03 <yangwenjie03@meituan.com>
2016-10-24 21:21:32 +0800

    new

---
+++ src/main/java/mir2/FirstMir2.java
@@ -0,0 +1,48 @@
+package mir2;
+
+import xnx3.microsoft.Com;
+import xnx3.microsoft.Window;
+import xnx3.robot.Robot;
+
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by yangwenjie on 16/10/24.
+ */
+public class FirstMir2 {
+    private static final Robot robot = new Robot();
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(1000*10);
+        Com com = new Com();
+        if(!com.isCreateSuccess()){
+            System.out.println("create com error");
+            return;
+        }
+
+        Window window=new Window(com.getActiveXComponent());
+        //查找当前运行的程序中标题包含“米尔”三个字的窗口句柄，
+        int hwnd=window.findWindow(0, null, "米尔");
+        //如果找到了寻仙这个游戏窗口了，确定是有这个程序存在，那么可以继续以下操作
+        if(hwnd>0) {
+			/*
+			 * 对找到的寻仙这个窗口进行绑定，绑定完毕后，那么所有的鼠标、键盘、图色等操作就是都是对这个窗口（寻仙游戏）操作的，完全后台的~~，窗口可以被遮挡、点击葫芦隐藏，但是不可以最小化
+			 * 寻仙的就是这种模式绑定，只需要传入窗口句柄就可
+			 * 其他游戏的请自行组合测试绑定模式,使用： com.bind(hwnd, display, mouse, key, mode) 自行测试
+			 */
+            if (com.bind(hwnd)) {
+                /**
+                 * 绑定完毕，对游戏的操作全在这里
+                 */
+                say("Hello, I am ou yang");
+            }
+        }
+    }
+
+    public static void say(String str){
+        robot.press((int) KeyEvent.KEY_EVENT_MASK);
+        for (int i = 0; i < str.length(); i++) {
+            robot.press(robot.charToKey(str.charAt(i)));
+        }
+        robot.press((int) KeyEvent.KEY_EVENT_MASK);
+    }
+}
---
+++ src/main/java/xnx3/j2ee/util/IpUtil.java
@@ -0,0 +1,43 @@
+package xnx3.j2ee.util;
+
+import com.xnx3.Lang;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * IP相关操作
+ * @author 管雷鸣
+ *
+ */
+public class IpUtil {
+	
+	/**
+	 * 获取IP地址，只会返回一个IP
+	 * @param request
+	 * @return <li>成功：返回一个ip
+	 * 			<li>失败：返回null
+	 */
+	public static String getIpAddress(HttpServletRequest request) {
+	    String ip = request.getHeader("x-forwarded-for");
+	    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+	        ip = request.getHeader("Proxy-Client-IP");  
+	    }  
+	    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+	        ip = request.getHeader("WL-Proxy-Client-IP");  
+	    }  
+	    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+	        ip = request.getRemoteAddr();  
+	    }  
+	    
+	    //如果有多个ip，拿最前面的
+	    if(ip.indexOf(",")>0){
+	    	ip=Lang.subString(ip, null, ",", 2);
+	    }
+	    
+	    if(ip.equals("0:0:0:0:0:0:0:1")){
+	    	ip = "127.0.0.1";
+	    }
+	    return ip;  
+	}   
+	
+}
