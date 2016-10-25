6356b5cb0061f051d0e5d25e53211f4bda4c6b0b
yangwenjie03 <yangwenjie03@meituan.com>
2016-10-25 19:02:19 +0800

    hhh

---
+++ src/main/java/RobotControlMir2Test.java
@@ -0,0 +1,22 @@
+import com.xnx3.robot.Robot;
+
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by yangwenjie on 16/10/25.
+ */
+public class RobotControlMir2Test {
+    private static final Robot robot = new Robot();
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(1000*10);
+        say("Hello Mir2, I am OuYang");
+    }
+
+    public static void say(String str){
+        robot.press((int) KeyEvent.KEY_EVENT_MASK);
+        for (int i = 0; i < str.length(); i++) {
+            robot.press(robot.StringToKey(str.substring(i, i+1)));
+        }
+        robot.press((int) KeyEvent.KEY_EVENT_MASK);
+    }
+}
