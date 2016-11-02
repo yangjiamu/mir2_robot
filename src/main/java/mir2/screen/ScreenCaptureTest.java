714edef2019cd4b450e38e2d8d73350333bc107e
Your Name <you@example.com>
2016-11-02 12:08:00 +0800

    all

--- src/main/java/mir2/screen/ScreenCaptureTest.java
+++ src/main/java/mir2/screen/ScreenCaptureTest.java
@@ -3,6 +3,7 @@ package mir2.screen;
 import mir2.robot.Robot2;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,9 +13,11 @@ import java.io.IOException;
  */
 public class ScreenCaptureTest {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Thread.sleep(1000 * 20);//wait for mir2 client to start and login
+        Thread.sleep(1000 * 0);//wait for mir2 client to start and login
         Robot2 robot2 = new Robot2();
         BufferedImage bufferedImage = robot2.captureScreen();
-        ImageIO.write(bufferedImage, "png", new File("C:/screen.png"));
+        ImageIO.write(bufferedImage, "png", new File("C:\\Users\\yang\\Pictures\\screen_captured1.png"));
+
+        System.out.println(Toolkit.getDefaultToolkit().getScreenSize().getWidth() + ":" + Toolkit.getDefaultToolkit().getScreenSize().getHeight());
     }
 }
